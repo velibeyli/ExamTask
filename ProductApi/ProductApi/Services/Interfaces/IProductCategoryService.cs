356e1fdc41e033f598e44efbc61210ad631d28e6
velibeyli <valibayli91@gmail.com>
2022-08-26 19:17:59 +0400

    created services

---
+++ ProductApi/ProductApi/Services/Interfaces/IProductCategoryService.cs
@@ -0,0 +1,13 @@
+﻿using ProductApi.Models;
+
+namespace ProductApi.Services.Interfaces
+{
+    public interface IProductCategoryService
+    {
+        Task<IEnumerable<ProductCategory>> Getall();
+        Task<ProductCategory> GetById(int id);
+        Task<ProductCategory> Create(ProductCategory category);
+        Task<ProductCategory> Update(int id, ProductCategory category);
+        Task<ProductCategory> Delete(int id);
+    }
+}
---
+++ ProductApi/ProductApi/Services/Interfaces/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using ProductApi.Models;
+
+namespace ProductApi.Services.Interfaces
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<Product>> GetAll();
+        Task<Product> GetById(int id);
+        Task<Product> Create(Product product);
+        Task<Product> Delete(int id);
+        Task<Product> Update(int id, Product product);
+    }
+}
