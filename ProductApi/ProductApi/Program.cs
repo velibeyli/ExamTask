c76bb7b569d3b02d07cb6eb9a884722548fd87e4
velibeyli <valibayli91@gmail.com>
2022-08-26 17:46:05 +0400

    created dbcontext

--- ProductApi/ProductApi/Program.cs
+++ ProductApi/ProductApi/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using ProductApi.Db;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+ConfigurationManager configuration = builder.Configuration;
+
+builder.Services.AddDbContext<ProductContext>(option => option.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
